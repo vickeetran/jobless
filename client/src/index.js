76b1734349a6c04f8ae000bda0e2eaf19503ddde
Alex <alex.lee3851@gmail.com>
2016-05-26 16:55:07 -0700

    require sass for webpack

--- client/src/index.js
+++ client/src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
 require('file?name=[name].[ext]!./index.html');
+require("!style!css!sass!./stylesheets/sass/style.scss");
 
 ReactDOM.render(<App />, document.getElementById('app'));
