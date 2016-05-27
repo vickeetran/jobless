c6c936c0add8f23ab37163c6297c2032117ce180
Alex <alex.lee3851@gmail.com>
2016-05-26 22:13:58 -0700

    added components to index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
+import JobList from './components/jobList.jsx';
+import JobView from './components/jobView.jsx';
+import EventView from './components/eventView.jsx';
 require('file?name=[name].[ext]!./index.html');
 require('./stylesheets/sass/style.scss');
 require('./stylesheets/sass/joblist.scss');
