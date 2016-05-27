7e2d9e90c0ba7d35f24f73a822b916a5b8215bc0
Sean Ho <seanthaoho@gmail.com>
2016-05-27 08:58:40 -0700

    added user fetching with redux

--- server/server.js
+++ server/server.js
@@ -7,7 +7,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true})) // get information from html forms
 app.use(express.static(__dirname + '/../client/public'));
 
-
+//for user testing
+// app.route('/user').get(function(req,res){
+//   console.log('/user');
+//   res.json({user: 'bob'});
+// });
 app.listen(port);
 console.log("listening to port: " + port)
 // module.exports = app
