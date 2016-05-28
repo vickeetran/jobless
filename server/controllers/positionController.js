dac17278f608bf858ea4ca11995de3cb0f3125ef
Alex <alex.lee3851@gmail.com>
2016-05-27 19:42:48 -0700

    close #47 eslint server/controller fix

--- server/controllers/positionController.js
+++ server/controllers/positionController.js
@@ -1,9 +1,12 @@
-var models = require('../models/positionModel');
+const models = require('../models/positionModel');
 
 module.exports = {
+  getAll: (req, res) => {
+    res.send();
+  },
   get: (req, res) => {
     models.get((err, results) => {
-      if(err) {
+      if (err) {
         console.log('error getting position data');
       }
       res.json(results);
@@ -12,19 +15,19 @@ module.exports = {
 
   post: (req, res) => {
     models.post(err => {
-      if(err) {
+      if (err) {
         console.log('error posting position data');
-      } 
+      }
       res.send(201);
-    }); 
+    });
   },
 
   put: (req, res) => {
     models.put(err => {
-      if(err) {
+      if (err) {
         console.log('error updating position data');
       }
       res.send(201);
     });
-  }
+  },
 };
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,9 +1,9 @@
-var models = require('../models/userModel');
+const models = require('../models/userModel');
 
 module.exports = {
   get: (req, res) => {
     models.get((err, results) => {
-      if(err) {
+      if (err) {
         console.log('error getting event data');
       }
       res.json(results);
@@ -12,19 +12,19 @@ module.exports = {
 
   post: (req, res) => {
     models.post(err => {
-      if(err) {
+      if (err) {
         console.log('error posting event data');
-      } 
+      }
       res.send(201);
-    }); 
+    });
   },
 
   put: (req, res) => {
     models.put(err => {
-      if(err) {
+      if (err) {
         console.log('error updating event data');
       }
       res.send(201);
     });
-  }
+  },
 };
