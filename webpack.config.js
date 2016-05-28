c9ee52815d3161305be94f6b8c71c9c24a6470fe
Alex <alex.lee3851@gmail.com>
2016-05-27 23:02:58 -0700

    eslint web pack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,36 @@
-var webpack = require('webpack');
-var path = require('path');
+const webpack = require('webpack');
+const path = require('path');
 
 module.exports = {
-    devtool: 'inline-source-map',
-    entry: [
-        'webpack-dev-server/client?http://127.0.0.1:8080/',
-        'webpack/hot/only-dev-server',
-        './client/src'
+  devtool: 'inline-source-map',
+  entry: [
+    'webpack-dev-server/client?http://127.0.0.1:8080/',
+    'webpack/hot/only-dev-server',
+    './client/src',
+  ],
+  output: {
+    path: path.join(__dirname, './client/public'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    modulesDirectories: ['node_modules', 'client/src'],
+    extensions: ['', '.js'],
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loaders: ['react-hot', 'babel?presets[]=react,presets[]=es2015'],
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'sass'],
+      },
     ],
-    output: {
-        path: path.join(__dirname, './client/public'),
-        filename: 'bundle.js'
-    },
-    resolve: {
-        modulesDirectories: ['node_modules', 'client/src'],
-        extensions: ['', '.js']
-    },
-    module: {
-        loaders: [
-          {
-            test: /\.jsx?$/,
-            exclude: /node_modules/,
-            loaders: ['react-hot', 'babel?presets[]=react,presets[]=es2015']
-          },
-          {
-            test: /\.scss$/,
-            loaders: ["style", "css", "sass"]
-          }
-        ]
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoErrorsPlugin()
-    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+  ],
 };
