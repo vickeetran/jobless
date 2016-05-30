6a1f9b9bf39406fbdde3bef371b9c4a09087f80c
Alex <alex.lee3851@gmail.com>
2016-05-29 18:54:36 -0700

    routes renaming

--- server/server.js
+++ server/server.js
@@ -3,13 +3,13 @@ const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
 // const db = require('./db');
-const router = require('./routes.js');
+const routes = require('./routes');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); // get information from html forms
 app.use(express.static(`${__dirname}/../client/public`));
 
-app.use('/api', router);
+app.use('/api', routes);
 
 app.listen(port);
 /* eslint-disable no-console */
