4d5245d25edef6e9ff0ba27031ba2f3d4e40ffdf
lcbits <lynnc24@gmail.com>
2016-06-03 22:37:33 -0700

    complete JobAdd Component

--- client/src/components/JobAdd.jsx
+++ client/src/components/JobAdd.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-//LYNN
 export default class JobAdd extends React.Component {
   constructor(props) {
     super(props);
@@ -11,9 +10,27 @@ export default class JobAdd extends React.Component {
 
   render() {
     return( 
-      <div className="container job-add">
-        <div className="row header">
-          <div className="col-xs-4 vcenter">Company</div>
+      <div id="form-main">
+        <div id="form-div">
+          <form className="form" id="form1">
+            <p className="inputForm">
+              <input type="text" className="validate[required,custom[onlyLetter],length[0,100]] feedback-input" placeholder="Company Name"/>
+            </p>
+            <p className="inputForm">
+              <input type="text" className="validate[required,custom[onlyLetter],length[0,100]] feedback-input" placeholder="Position Tile"/>
+            </p>
+            <p className="text">
+              <textarea name="text" className="validate[required,length[6,300]] feedback-input" id="comment" placeholder="Job Description"></textarea>
+            </p>
+            
+            <p className="text">
+              <textarea name="text" className="validate[required,length[6,300]] feedback-input" id="comment" placeholder="Notes"></textarea>
+            </p>
+            <div className="submit">
+              <input type="submit" value="ADD NEW POSITION" id="button-blue"/>
+              <div className="ease"></div>
+            </div>
+          </form>
         </div>
       </div>
     );
