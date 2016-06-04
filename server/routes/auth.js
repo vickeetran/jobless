8177aba78e32f6358cdd034f5a33b5ba0d075ee0
Alex <alex.lee3851@gmail.com>
2016-06-03 18:56:38 -0700

    change passport configuration path

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line new-cap
 const router = require('express').Router();
-const passport = require('../helpers/passport');
+const passport = require('../config/passport');
 
 router.get('/login', passport.authenticate('google', { scope: ['profile', 'email'] }));
 router.get('/google/callback', passport.authenticate('google', {
