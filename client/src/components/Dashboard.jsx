9547549f435875f13d7aae101d6d14d4e0f30044
lcbits <lynnc24@gmail.com>
2016-06-05 15:21:07 -0700

    finalize dashboard

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -89,7 +89,7 @@ export default class Dashboard extends React.Component {
           <div className="row">
               <div className="col-xs-5 col-md-5 left-container container">
                 <div className="menu-box block"> 
-                    <h2 className="titular">Job Apps
+                    <h2 className="titular">Active Job Apps
                       <button type="button" className="btn btn-default btn-xs">
                         <span className="glyphicon glyphicon-plus" aria-hidden="true" onClick={this.openModalAdd}></span>
                       </button>
@@ -101,84 +101,36 @@ export default class Dashboard extends React.Component {
               </div>
               <div className="col-xs-7 col-md-7 left-container container">
                 <div className="menu-box block"> 
-                  <h2 className="titular">Todo<button type="button" className="btn btn-default btn-xs"><span className="glyphicon glyphicon-plus" aria-hidden="true"></span>
+                  <h2 className="titular">Today's Appointments<button type="button" className="btn btn-default btn-xs">
   </button></h2>
                     <ul className="menu-box-menu">
                       {events.map(event => <TodoListEntry key={event.id} data={event}/>)}
                     </ul>
                 </div>
-                <div className="calendar-month block"> 
+                <div className="jobPosting-month block"> 
                   <div className="arrow-btn-container">
                     <a className="arrow-btn left" href="#202"><span className="icon fontawesome-angle-left"></span></a>
-                    <h2 className="titular">June 2016</h2>
+                    <h2 className="titular">Job Depot</h2>
                     <a className="arrow-btn right" href="#203"><span className="icon fontawesome-angle-right"></span></a>
                   </div>
-                  <table className="calendar">
+                  <table className="jobPosting">
                     <thead className="days-week">
                         <tr>
-                            <th>S</th>
-                            <th>M</th>
-                            <th>T</th>
-                            <th>W</th>
-                            <th>R</th>
-                            <th>F</th>
-                            <th>S</th>
+                            <th>Company</th>
+                            <th>Position</th>
+                            <th>Job Posting</th>
                         </tr>
                     </thead>
                     <tbody>
                         <tr>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td><a className="scnd-font-color" href="#100">1</a></td>
+                            <td>Yelp</td>
+                            <td>Software Engineer</td>
+                            <td><a href="http://www.yelp.com/careers/job-openings/cdefb0de-1998-4814-b459-c4417ef94efc?description=Software-Engineer-New-Grad_College-Engineering-Product_San-Francisco-CA&lever-source=indeed">Product Development Team</a></td>
                         </tr>
                         <tr>
-                            <td><a className="scnd-font-color" href="#101">2</a></td>
-                            <td><a className="today" href="#102">3</a></td>
-                            <td><a className="scnd-font-color" href="#103">4</a></td>
-                            <td><a className="scnd-font-color" href="#104">5</a></td>
-                            <td><a className="scnd-font-color" href="#105">6</a></td>
-                            <td><a className="scnd-font-color" href="#106">7</a></td>
-                            <td><a className="scnd-font-color" href="#107">8</a></td>
-                        </tr>
-                        <tr>
-                            <td><a className="scnd-font-color" href="#108">9</a></td>
-                            <td><a className="scnd-font-color" href="#109">10</a></td>
-                            <td><a className="scnd-font-color" href="#110">11</a></td>
-                            <td><a className="scnd-font-color" href="#111">12</a></td>
-                            <td><a className="scnd-font-color" href="#112">13</a></td>
-                            <td><a className="scnd-font-color" href="#113">14</a></td>
-                            <td><a className="scnd-font-color" href="#114">15</a></td>
-                        </tr>
-                        <tr>
-                            <td><a className="scnd-font-color" href="#115">16</a></td>
-                            <td><a className="scnd-font-color" href="#116">17</a></td>
-                            <td><a className="scnd-font-color" href="#117">18</a></td>
-                            <td><a className="scnd-font-color" href="#118">19</a></td>
-                            <td><a className="scnd-font-color" href="#119">20</a></td>
-                            <td><a className="scnd-font-color" href="#120">21</a></td>
-                            <td><a className="scnd-font-color" href="#121">22</a></td>
-                        </tr>
-                        <tr>
-                            <td><a className="scnd-font-color" href="#122">23</a></td>
-                            <td><a className="scnd-font-color" href="#123">24</a></td>
-                            <td><a className="scnd-font-color" href="#124">25</a></td>
-                            <td><a className="scnd-font-color" href="#125">26</a></td>
-                            <td><a href="#126">27</a></td>
-                            <td><a href="#127">28</a></td>
-                            <td><a href="#128">29</a></td>
-                        </tr>
-                        <tr>
-                            <td><a href="#129">30</a></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
+                            <td>Pinterest</td>
+                            <td>Software Engineer</td>
+                            <td><a href="http://jobs.bvp.com/jobdetail.php?jobid=526487">Community Ops and Trust & Safety Team</a></td>
                         </tr>
                       </tbody>
                   </table>
