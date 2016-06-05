aa4d93f5392fd0a3861fa93e5de7710dbc4070f0
Alex <alex.lee3851@gmail.com>
2016-06-05 04:01:01 -0700

    create new user if doesn't exist in  database and attaches events to req.user session

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,26 +1,37 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 const db = require('./../db');
+const config = require('./client_secret');
+const calendar = require('../helpers/calendar');
 
 passport.use(new GoogleStrategy({
-  clientID: '698335281879-q1ogep3olou0o0vhn0i9383irnbime8b.apps.googleusercontent.com',
-  clientSecret: 'wRSinYF2x4eRqa2TsYAwBF8o',
-  callbackURL: 'http://localhost:3000/auth/google/callback',
+  clientID: config.web.client_id,
+  clientSecret: config.web.client_secret,
+  callbackURL: config.web.redirect_uris[0],
 },
 (token, refreshToken, profile, done) => {
   process.nextTick(() => {
-    done(null, profile);
+    calendar.events(token, 'primary')
+      // Add calendar to profile
+      .then(events => calendar.extendWithCalendar(profile, events))
+      .then(fullProfile => done(null, fullProfile));
   });
 }));
 
-passport.serializeUser((user, done) => {
-  console.log(JSON.stringify(user));
-  done(null, user);
+passport.serializeUser((profile, done) => {
+  // console.log(JSON.stringify(profile, null, 2));
+  done(null, profile);
 });
 
 // used to deserialize the user
-passport.deserializeUser((id, done) => {
-  db.User.findOne({ where: { id: 1 } })
+passport.deserializeUser((profile, done) => {
+  db.User.findOrCreate({ where: {
+    googleID: profile.id,
+    email: profile.emails[0].value,
+    firstName: profile.name.givenName,
+    lastName: profile.name.familyName,
+  } })
+    .then(user => calendar.extendWithCalendar(user, profile.calendarEvents))
     .then(user => done(null, user))
     .catch(err => done(err));
 });
