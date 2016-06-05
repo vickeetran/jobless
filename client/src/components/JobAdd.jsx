45a2b906963757429356ebcf8261736d0a5a0251
lcbits <lynnc24@gmail.com>
2016-06-05 10:41:40 -0700

    complete jobadd form

--- client/src/components/JobAdd.jsx
+++ client/src/components/JobAdd.jsx
@@ -1,32 +1,36 @@
 import React from 'react';
+
 import Dashboard from './Dashboard.jsx';
 
 export default class JobAdd extends React.Component {
   constructor(props) {
     super(props);
+    this.methods = props.methods;
+  }
 
-    this.state = {
-      
+  handleSubmit (event) {
+    event.preventDefault();
+    var formData = {
+      title: this.refs.title.value,
+      company: this.refs.company.value,
+      description: this.refs.description.value
     }
+    this.methods.postJob(formData);
   }
 
   render() {
     return( 
       <div id="form-main">
         <div id="form-div">
-          <form className="form" id="form1">
+          <form className="form" id="form1" onSubmit={this.handleSubmit.bind(this)}>
             <p className="inputForm">
-              <input type="text" className="validate[required,custom[onlyLetter],length[0,100]] feedback-input" placeholder="Company Name"/>
+              <input type="text" ref="company" className="validate[required,custom[onlyLetter],length[0,100]] feedback-input" placeholder="Company Name"/>
             </p>
             <p className="inputForm">
-              <input type="text" className="validate[required,custom[onlyLetter],length[0,100]] feedback-input" placeholder="Position Tile"/>
+              <input type="text" ref="title" className="validate[required,custom[onlyLetter],length[0,100]] feedback-input" placeholder="Position Tile"/>
             </p>
             <p className="text">
-              <textarea name="text" className="validate[required,length[6,300]] feedback-input" id="comment" placeholder="Job Description"></textarea>
-            </p>
-            
-            <p className="text">
-              <textarea name="text" className="validate[required,length[6,300]] feedback-input" id="comment" placeholder="Notes"></textarea>
+              <textarea name="text" ref="description" className="validate[required,length[6,300]] feedback-input" id="comment" placeholder="Job Description"></textarea>
             </p>
             <div className="submit">
               <input type="submit" value="ADD NEW POSITION" id="button-blue"/>
